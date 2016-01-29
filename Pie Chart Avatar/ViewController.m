4b071b03df89280d22ac79979df78d0209274aa5
Hamish Knight <hk@southerncare.co.uk>
2016-01-29 23:01:04 +0000

    example of animating adding extra portions

--- Pie Chart Avatar/ViewController.m
+++ Pie Chart Avatar/ViewController.m
@@ -20,17 +20,28 @@
     avatarView.avatarImage = [UIImage imageNamed:@"photo.png"];
     avatarView.borderWidth = 10;
     avatarView.borderColors = @[[UIColor colorWithRed:122.0/255.0 green:108.0/255.0 blue:255.0/255.0 alpha:1],
-                       [UIColor colorWithRed:100.0/255.0 green:241.0/255.0 blue:183.0/255.0 alpha:1],
-                       [UIColor colorWithRed:0 green:222.0/255.0 blue:255.0/255.0 alpha:1]];
-    avatarView.borderValues = @[@(0), @(0), @(0)];
+                                [UIColor colorWithRed:100.0/255.0 green:241.0/255.0 blue:183.0/255.0 alpha:1],
+                                [UIColor colorWithRed:0 green:222.0/255.0 blue:255.0/255.0 alpha:1],
+                                [UIColor colorWithRed:1 green:138.0/255.0 blue:0 alpha:1],
+                                [UIColor colorWithRed:1 green:0 blue:102.0/255.0 alpha:1]
+                                ];
+    avatarView.borderValues = @[@(0.4), @(0.35), @(0.25), @(0), @(0)];
     
     [self.view addSubview:avatarView];
     
 }
 
+-(void) viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    
+   // [avatarView animateToBorderValues:@[@(0.4), @(0.35), @(0.25)] duration:2];
+    
+}
+
 -(void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     
-    [avatarView animateToBorderValues:@[@(0.4), @(0.35), @(0.25)] duration:2];
+    [avatarView animateToBorderValues:@[@(0.1), @(0.2), @(0.3), @(0.25), @(0.15)] duration:2];
     
 }
 
